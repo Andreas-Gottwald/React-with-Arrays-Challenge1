ef9c5b4e6a810d10dd37d59fc0bfcc32b09e3f98
Andreas Gottwald <andreas.gottwald@hotmail.de>
2023-12-28 12:13:27 +0100

    finish

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import Card from "./components/Card";
+import { Fragment } from "react";
 
 export default function App() {
-  const fruits = [];
+  const fruits = [
+    { id: 1, name: "banana", color: "yellow" },
+    { id: 2, name: "apple", color: "red" },
+    { id: 3, name: "cucumber", color: "green" },
+    { id: 4, name: "mango", color: "orange" },
+    { id: 5, name: "plum", color: "purple" },
+  ];
 
   return (
     <div className="app">
-      <Card name="🍌 banana" />
+      {fruits.map(({ id, name, color }) => (
+        <Fragment key={id}>
+          <Card name={name} color={color} />
+        </Fragment>
+      ))}
     </div>
   );
 }
